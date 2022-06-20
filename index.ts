b563ed6295fde6e3242815d1771fca4585364e48
Brian Stadnicki <stadnickibrian@gmail.com>
2022-06-20 21:46:32 +0100

    allow viewing reviews

--- index.ts
+++ index.ts
@@ -23,12 +23,20 @@ function renderReview(review) {
         <div id="review-${review['id']}" class="card col-5 mb-2">
             <div class="card-header">
                 <h1>${review['subject']}
-                    <button class="btn btn-secondary float-end" onclick="modifyReviewButton(${review['id']})">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                        </svg>
-                    </button>
+                    <div class="float-end">
+                        <button class="btn btn-secondary me-2" onclick="modifyReviewButton(${review['id']})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                            </svg>
+                        </button>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-review-${review['id']}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                            </svg>
+                        </button>
+                    </div>
                 </h1>
                 <hr>
                 <h3>${review['topic']}</h3>
@@ -37,6 +45,25 @@ function renderReview(review) {
             </div>
             <div class="card-body">
                 <b>${review['content']}</b>
+            </div>
+            <div class="modal fade" id="modal-review-${review['id']}" tabindex="-1" aria-labelledby="modal-review-label-${review['id']}" aria-hidden="true">
+                <div class="modal-dialog modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1>${review['subject']}</h1>     
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>                       
+                        </div>
+                        <div class="modal-body">
+                            <h3>${review['topic']}</h3>
+                            <hr>
+                            <h5>${review['sub-topic']}</h5>
+                            <hr>
+                            <b>${review['content']}</b>
+                            <hr>
+                            <p style="white-space: pre-wrap;">${review['additional-information']}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         `
