ac48bb9912357ed35d69a75152940b973ab04bcf
Brian Stadnicki <stadnickibrian@gmail.com>
2022-06-24 00:11:52 +0100

    major simplification

--- index.ts
+++ index.ts
@@ -1,211 +1,153 @@
+enum ReviewStatus {
+    NotDone,
+    PartlyDone,
+    Done
+}
+
 window.onload = function () {
-
-    document.getElementById('new-review-form').addEventListener('submit', addReview)
-
     if (!localStorage.getItem('reviews')) {
         localStorage.setItem('reviews', JSON.stringify([]));
     } else {
         loadReviews();
     }
-};
+}
 
 function loadReviews() {
-    document.getElementById('reviews-list').innerHTML = "";
-
     let reviews = JSON.parse(localStorage.getItem('reviews'))
         .map(id => JSON.parse(localStorage.getItem('review-' + id)))
         .sort(function (a, b) {return a['next-attempt'] - b['next-attempt']})
         .reduce((group, review) => {
             const date = new Date(review['next-attempt']).toLocaleDateString();
-            group[date] = [...group[date] ?? [], review];
+            const subject = review['subject'];
+            group[date] = group[date] ?? {};
+            group[date][subject] = [...group[date][subject] ?? [], review]
             return group;
         }, {});
 
-    document.getElementById('reviews-list').innerHTML =
+    document.getElementById('reviews').innerHTML =
         Object.keys(reviews)
-        .map(date => {
-            return `
-            <div id="reviews-date-${date}" class="card">
-                <div class="card-header">
-                    <h1 class="fs-3">${date}</h1>
-                </div>
-                <div class="card-body row row-cols-2">
-                    ${reviews[date].map(review => renderReview(review)).join('')}
-                </div>
-            </div>
-            `
-        })
-        .join('');
-}
-
-function renderReview(review) {
-    return `
-        <div id="review-${review['id']}" class="card col col-6 mb-2">
-            <div class="card-header">
-                <h1>${review['subject']}
-                    <div class="float-end fs-4">
-                        <button class="btn btn-danger me-2" data-bs-toggle="modal" data-bs-target="#delete-review-modal-${review['id']}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>
-                        </button>
-                        <button class="btn btn-secondary me-2" data-bs-toggle="modal" data-bs-target="#edit-review-modal-${review['id']}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>
-                        </button>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-review-${review['id']}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16"><path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/><path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/></svg>
-                        </button>
+            .map(date => {
+                return `
+                <div id="reviews-date-${date}" class="card mb-2">
+                    <div class="card-header">
+                        <h1 class="fs-3">${date}</h1>
                     </div>
-                </h1>
-                <hr>
-                <h3>${review['topic']}</h3>
-                <hr>
-                <h5>${review['sub-topic']}</h5>
-            </div>
-            <div class="card-body">
-                <b>${review['content']}</b>
-            </div>
-            <div class="modal fade" id="modal-review-${review['id']}" tabindex="-1" aria-labelledby="modal-review-label-${review['id']}" aria-hidden="true">
-                <div class="modal-dialog modal-xl">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <div class="container row">
-                                <h1 class="col col-7">${review['subject']}</h1>
-                                <h5 class="col col-5">Next attempt:<br>${new Date(review['next-attempt']).toLocaleDateString()}</h5>
-                            </div>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>                       
-                        </div>
-                        <div class="modal-body">
-                            <h3>${review['topic']}</h3>
-                            <hr>
-                            <h5>${review['sub-topic']}</h5>
-                            <hr>
-                            <b>${review['content']}</b>
-                            <hr>
-                            <p style="white-space: pre-line">${review['additional-information']}</p>
-                            <hr>
-                            <p style="white-space: pre-line;">${review['resources']}</p>
-                        </div>
+                    <div class="card-body">
+                        ${Object.keys(reviews[date]).map(subjectKey => {
+                            let subject = reviews[date][subjectKey];
+                            return `
+                                <div class="row mb-2 p-2 border rounded">
+                                    <div class="col col-12 col-md-2 col-xl-1 mb-2 border rounded">
+                                        ${subject[0]['subject']}
+                                    </div>
+                                    <div class="col">
+                                        <ul class="list-group">
+                                            ${subject.map(review => {
+                                                return `
+                                                    <li class="list-group-item vstack gap-2">
+                                                        <div>
+                                                            <div class="float-start">${review['title']}</div>
+                                                            <button class="btn btn-sm btn-primary float-end">
+                                                                <img src="icons/check2-square.svg" alt="check">
+                                                            </button>
+                                                            <button class="btn btn-sm btn-secondary float-end me-2">
+                                                                <img src="icons/pencil-square.svg" alt="edit">
+                                                            </button>
+                                                            ${ review['resources'].length !== 0 ? `
+                                                                <button class="btn btn-sm btn-outline-primary float-end me-2" data-bs-toggle="collapse" data-bs-target="#review-collapse-${review['id']}">
+                                                                    Resources
+                                                                </button>
+                                                            ` : '' }
+                                                            
+                                                        </div>
+                                                        ${review['resources'].length !== 0 ? `
+                                                            <div class="collapse" id="review-collapse-${review['id']}">
+                                                                <div class="card card-body">
+                                                                    ${review['resources'].map(resource => {
+                                                                        if (resource['type'] === "url") {
+                                                                            return `<a href="${resource['location']}">${resource['location']}</a>`
+                                                                        } else {
+                                                                            return `<span>${resource['location']}</span>`
+                                                                        }
+                                                                    }).join('')}
+                                                                </div>
+                                                            </div>
+                                                        ` : '' }
+                                                    </li>
+                                                `
+                                            }).join('')}
+                                        </ul>
+                                    </div>
+                                </div>        
+                            `
+                        }).join('')}
                     </div>
                 </div>
-            </div>
-            <div class="modal fade" id="edit-review-modal-${review['id']}" tabindex="-1" aria-labelledby="edit-review-modal-label-${review['id']}" aria-hidden="true">
-                <div class="modal-dialog modal-xl">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="edit-review-modal-label-${review['id']}">Edit review</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <form id="edit-review-form-${review['id']}" role="form" data-lpignore="true" onsubmit="modifyReview(this, ${review['id']})">
-                            <div class="modal-body">
-                                <div class="mb-3 form-floating">
-                                    <input type="text" class="form-control" id="subject" placeholder="N/A" value="${review['subject']}" required>
-                                    <label for="subject" class="form-label">Subject</label>
-                                </div>
-                                <div class="mb-3 form-floating">
-                                    <input type="text" class="form-control" id="topic" placeholder="N/A" value="${review['topic']}" required>
-                                    <label for="topic" class="form-label">Topic</label>
-                                </div>
-                                <div class="mb-3 form-floating">
-                                    <input type="text" class="form-control" id="sub-topic" placeholder="N/A" value="${review['sub-topic']}" required>
-                                    <label for="sub-topic" class="form-label">Sub-topic</label>
-                                </div>
-                                <div class="mb-3 form-floating">
-                                    <textarea class="form-control" id="content" placeholder="N/A" required>${review['content']}</textarea>
-                                    <label for="content" class="form-label">Content</label>
-                                </div>
-                                <div class="mb-3 form-floating">
-                                    <textarea class="form-control" id="additional-information" placeholder="N/A" rows="10">${review['additional-information']}</textarea>
-                                    <label for="additional-information" class="form-label">Additional information</label>
-                                </div>
-                                <div class="mb-3 form-floating">
-                                    <textarea class="form-control" id="resources" placeholder="N/A" rows="5" aria-describedby="resourcesHelp">${review['resources'].split('\n').map(str => str.match('(?<=<a href=")(?:.)*(?=">)') ?? str).join('\n')}</textarea>
-                                    <label for="resources" class="form-label">Resources</label>
-                                    <div id="resourcesHelp" class="form-text">Links must be on separate lines</div>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button id="edit-review-modal-close-btn-${review['id']}" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button id="edit-review-modal-submit-btn-${review['id']}" type="submit" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="modal fade" id="delete-review-modal-${review['id']}" tabindex="-1" aria-labelledby="delete-review-modal-label-${review['id']}" aria-hidden="true">
-                <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="delete-review-modal-label-${review['id']}">Are you sure you want to delete this review?</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-footer">
-                            <button id="delete-review-modal-close-btn-${review['id']}" type="button" class="btn btn-primary col" data-bs-dismiss="modal">Close</button>
-                            <button id="delete-review-modal-submit-btn-${review['id']}" type="button" onclick="deleteReview(${review['id']})" class="btn btn-danger col">Delete review</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `
+                `
+            })
+            .join('');
 }
 
-function addReview() {
-    document.getElementById('create-review-model-submit-btn')["disabled"] = true;
+function formNewReview(form: HTMLFormElement) {
+    form['disabled'] = true;
 
-    let review = createReview(<HTMLFormElement>document.getElementById('new-review-form'));
-
-    localStorage.setItem('reviews', JSON.stringify(JSON.parse(localStorage.getItem('reviews')).concat(review.id)));
-    localStorage.setItem('review-' + review.id, JSON.stringify(review));
-
-    loadReviews();
-
-    document.getElementById("create-review-model-close-btn").click();
-    setTimeout(() => {
-        document.getElementById('create-review-model-submit-btn')["disabled"] = false;
-    }, 1000)
-}
-
-function modifyReview(form: HTMLFormElement, id: number) {
-    let previousReview = JSON.parse(localStorage.getItem('review-' + id));
-    let review = createReview(form);
-
-    review['id'] = previousReview['id'];
-    review['next-attempt'] = previousReview['next-attempt'];
-    review['attempts'] = previousReview['attempts'];
-    localStorage.setItem('review-' + review.id, JSON.stringify(review));
-
-    document.getElementById("edit-review-modal-close-btn-" + id).click();
-
-    loadReviews();
-}
-
-function deleteReview(id: number) {
-    localStorage.removeItem('review-' + id);
-    localStorage.setItem('reviews', JSON.stringify(JSON.parse(localStorage.getItem('reviews')).filter(a => a !== id)));
-
-    document.getElementById("delete-review-modal-close-btn-" + id).click();
-
-    loadReviews();
-}
-
-function createReview(form: HTMLFormElement) {
     let reviews = JSON.parse(localStorage.getItem('reviews'));
+
     let id = 0;
     if (reviews.length != 0) {
         id = reviews.at(-1) + 1;
     }
 
-    let now = new Date();
+    let resources = [];
+    let resourceCounter = 0;
+    while (form.elements['resource-location-' + resourceCounter]) {
+        if (form.elements['resource-location-' + resourceCounter].value !== "") {
+            resources.push({
+                'location': form.elements['resource-location-' + resourceCounter].value,
+                'type': form.elements['resource-type-' + resourceCounter].value,
+                'status': ReviewStatus.NotDone
+            })
+        }
+        resourceCounter += 1;
+    }
 
-    return {
+    let date = new Date();
+    date.setDate(date.getDate() + 1);
+
+    let review = {
         'id': id,
+        'title': form.elements['title'].value,
         'subject': form.elements['subject'].value,
-        'topic': form.elements['topic'].value,
-        'sub-topic': form.elements['sub-topic'].value,
-        'content': form.elements['content'].value,
-        'additional-information': form.elements['additional-information'].value,
-        'resources': form.elements['resources'].value.split('\n').map(str => (str.startsWith('http://') || str.startsWith('https://')) ? `<a href="${str}">${str}</a>` : str).join('\n'),
-        'created': Date.now(),
-        'next-attempt': now.setDate(now.getDate() + 1),
-        'attempts': []
-    };
+        'resources': resources,
+        'next-attempt': date.valueOf()
+    }
+
+    localStorage.setItem('reviews', JSON.stringify(JSON.parse(localStorage.getItem('reviews')).concat(review.id)));
+    localStorage.setItem('review-' + review.id, JSON.stringify(review));
+
+    form['disabled'] = false;
+    document.getElementById('new-review-form-close-btn').click();
+    loadReviews();
+}
+
+let newReviewResourcesCount = 0;
+
+function formNewReviewAddResource() {
+    document.getElementById('modal-new-review-resources').insertAdjacentHTML('beforeend',
+        `
+        <div class="mb-3 row g-0">
+            <div class="col-sm-2">
+                <select class="form-select" id="resource-type-${newReviewResourcesCount}">
+                    <option value="url" selected>URL</option>
+                    <option value="plain">Plain</option>
+                </select>
+            </div>
+            <div class="col-sm">
+                <input type="text" class="form-control" id="resource-location-${newReviewResourcesCount}">
+            </div>
+        </div>
+        `
+    );
+
+    newReviewResourcesCount += 1;
 }
